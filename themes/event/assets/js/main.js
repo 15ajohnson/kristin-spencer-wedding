f173ca9e06f1a1f42cb4340ccc4a712fcd9ee622
15ajohnson <aliviaj@bgsu.edu>
2025-02-02 14:19:31 -0500

    Option: Moving nav up top and made sticky

--- themes/event/assets/js/main.js
+++ themes/event/assets/js/main.js
@@ -5,8 +5,8 @@
   })();
     
   function scrollFunction() {
-    document.getElementById("menu-icon-active").setAttribute("id", "menu-icon-active")
-    // if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      console.log(document.documentElement.scrollTop)
     //   // Scroll Down
     //   document.getElementById("header").style.position="fixed";
     //   document.getElementById("header").style.top="0px";
@@ -14,12 +14,12 @@
     //   document.getElementById("header-content").style.overflow="hidden";
     //   // document.getElementById("logo").style.width="100px";
     //   // document.getElementById("sub-header").style.display="none";
-    // } else {
+    } else {
     //   // document.getElementById("logo").style.width="150px";
     //   // document.getElementById("sub-header").style.display="block";
     //   document.getElementById("header").style.position="absolute";
     //   document.getElementById("header").style.top="20px";
     //   document.getElementById("header-content").style.height="275px";
     //   document.getElementById("header-content").style.overflow="visible";
-    // }
+    }
   }
